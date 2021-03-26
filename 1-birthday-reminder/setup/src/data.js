2c24f2acf8462e025f15b592ee3ef3c39adb2f7f
Marcos <mandaluzr@hotmail.com>
2021-03-26 12:36:18 +0100

    birtdhay-reminder-done

--- 1-birthday-reminder/setup/src/data.js
+++ 1-birthday-reminder/setup/src/data.js
@@ -1,36 +1,36 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
-    age: 29,
+    name: 'Flor Nakis',
+    age: 28,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
     id: 2,
-    name: 'Hester Hogan',
-    age: 32,
+    name: 'Rosario Soto',
+    age: 26,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
   {
     id: 3,
-    name: 'Larry Little',
-    age: 36,
+    name: 'Máximo Borsani',
+    age: 30,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
   {
     id: 4,
-    name: 'Sean Walsh',
-    age: 34,
+    name: 'Marcos Andaluz de Rosas',
+    age: 27,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
   {
     id: 5,
-    name: 'Lola Gardner',
-    age: 29,
+    name: 'Milagros Andaluz',
+    age: 33,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
   },
